1c4cc3fef1fdbaf8f911d466ba0be7be61a0a621
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2019-11-12 16:49:03 +0100

    tp03

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -11,9 +12,18 @@ public class App
     public static void main( String[] args )
     {
     	
-    	String banner = FigletFont.convertOneLine("App");
+    /*	String banner = FigletFont.convertOneLine("App");
         System.out.println( banner );
         
+        */
+     String titre = ResourceBundle.getBundle("application").getString("titre"); 
+     String asciiArt = FigletFont.convertOneLine(titre);
+     System.out.println(asciiArt);
         
+     
+     // affichage de l'environnement
+     String environnement = ResourceBundle.getBundle("application").getString("environnement");
+     System.out.println("Environnement : " + environnement);
     }
+    
 }
